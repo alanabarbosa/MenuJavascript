515967bceef4ef8a1e4d57db6e63d3fc1c8bd1e4
alanabarbosa <itsalanayall@hotmail.com>
2022-02-03 11:22:38 -0300

    Adiciona melhorias ao codigo js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-		function funcaoChance(elemento){
-			if(elemento.value == 1){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>Tradicional</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20Hamburguer%20Tradcional">Faça seu pedido</a></div>  <br/> <img src="img/tradicional.png"> <br/>  '
-			}
-			else if(elemento.value == 2){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>x-Bacon</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20x-Bacon">Faça seu pedido</a></div>  <br/> <img src="img/xbacon.png"> <br/>  '			
-			}
-			else if(elemento.value == 3){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>x-Calabresa</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20x-Calabresa">Faça seu pedido</a></div>  <br/> <img src="img/xcalabresa.png"> <br/>  '	
-			}
-			else if(elemento.value == 4){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>x-Tudo</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20x-Tudo">Faça seu pedido</a></div>  <br/> <img src="img/xtudo.png"> <br/>  '
-			}
-			else if(elemento.value == 5){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>x-Cheddar</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20x-Cheddar">Faça seu pedido</a></div>  <br/> <img src="img/xcheddar.png"> <br/>  '	
-			}
-			else if(elemento.value == 6){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>x-Chicken</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20x-Chicken">Faça seu pedido</a></div>  <br/> <img src="img/xchiken.png"> <br/>  '	
-			}
-			else if(elemento.value == 7){			
-			document.getElementById("Pedido").innerHTML ='<div><h1>Super Combo </h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><a href="https://api.whatsapp.com/send?phone=551991737267&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%20do%20Super%20combo">Faça seu pedido</a></div>  <br/> <img src="img/combo.png"> <br/>  '		
-			}
-		}
+const selectValue = document.querySelector("#SelectOption");
+const produtoDisplay = document.querySelector(".product");
+const boxProduto = document.querySelector(".Pedido");
+const innerProductBox = boxProduto.getElementsByTagName("div");
+
+selectValue.addEventListener("change", () => {
+    valueSelect = selectValue.options[selectValue.selectedIndex].value;
+    removeBox()
+    showBox(selectValue.selectedIndex);
+    produtoDisplay.innerText = valueSelect;
+});
+
+function removeBox() {
+    for (let div of innerProductBox) {
+        div.style.display = "none";
+    }
+}
+
+function showBox(index) {
+    const currentBox = boxProduto.children[index - 1];
+    currentBox.style.display = "block";
+}
